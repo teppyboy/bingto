a37df1722c22e3550a5f48a28b5d93b17d53dbb8
tretrauit <tretrauit@gmail.com>
2023-11-13 23:44:01 +0700

    fix: don't fail if playwright_stealth failed to import
    Python 3.12 owo, also migrated to logging

--- bingto/app.py
+++ bingto/app.py
@@ -1,4 +1,6 @@
 import argparse
+import logging
+import sys
 from bingto import __version__
 from bingto.constant import EDGE_IOS_UA, WORD_LIST
 from playwright.sync_api import (
@@ -14,8 +16,35 @@ from pathlib import Path
 from time import sleep
 from random import choice, randint, uniform
 from runpy import run_module
-from playwright_stealth import stealth_sync
-import sys
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s %(name)s (%(funcName)s) (%(filename)s:%(lineno)d) [%(levelname)s] %(message)s",
+)  # noqa: E501
+
+
+fake_playwright_stealth_init = False
+def init_fake_playwright_stealth():
+    global fake_playwright_stealth_init
+    if fake_playwright_stealth_init:
+        return
+    fake_playwright_stealth_init = True
+    logging.warning("Using dummy implementations for playwright_stealth...")
+    logging.warning("Bingto may be easier to be detected, use at your own risk.")
+    global stealth_sync
+
+    def stealth_sync(page: Page):
+        logging.warning("stealth_sync() called.")
+        pass
+
+
+try:
+    from playwright_stealth import stealth_sync
+except ImportError:
+    logging.warning(
+        "playwright_stealth failed to import."
+    )
+    init_fake_playwright_stealth()
 
 
 DEBUG = False
@@ -41,31 +70,10 @@ class Debug:
     @staticmethod
     def print(*args, **kwargs):
         """
-        Print if DEBUG is True.
+        logging.info if DEBUG is True.
         """
         if DEBUG:
-            print("[DEBUG]:", *args, **kwargs)
-
-
-def dbg_pause():
-    """
-    Pause the program if DEBUG is True.
-    """
-    if DEBUG:
-        input("[DEBUG]: Press [ENTER] to continue execution.")
-
-
-def dbg_screenshot(page: Page, name: str):
-    """
-    Take a screenshot if DEBUG is True.
-    """
-    if DEBUG:
-        page.screenshot(path=f"{name}.png")
-
-
-def dbg_print(*args, **kwargs):
-    if DEBUG:
-        print("[DEBUG]:", *args, **kwargs)
+            logging.info("[DEBUG]:", *args, **kwargs)
 
 
 def wait(a: float, b: float):
@@ -75,15 +83,17 @@ def wait(a: float, b: float):
     sleep(uniform(a, b))
 
 
-def create_browser(p: Playwright, headless: bool, browser_type: BrowserType = None) -> Browser:
+def create_browser(
+    p: Playwright, headless: bool, browser_type: BrowserType = None
+) -> Browser:
     try:
         if browser_type:
             browser = browser_type.launch(headless=headless, channel="msedge")
         else:
             browser = p.chromium.launch(headless=headless, channel="msedge")
     except Error as e:
-        print(f"Error occurred while launching Edge: {e}")
-        print("Trying to launch Chromium...")
+        logging.info(f"Error occurred while launching Edge: {e}")
+        logging.info("Trying to launch Chromium...")
         if browser_type:
             browser = browser_type.launch(headless=headless)
         else:
@@ -95,8 +105,8 @@ def login():
     """
     Initiate the login process for Bing.
     """
-    print("This will initiate the login process for Bing.")
-    print("After logging in, press [ENTER] to continue.", end="")
+    logging.info("This will initiate the login process for Bing.")
+    logging.info("After logging in, press [ENTER] to continue.", end="")
     with sync_playwright() as p:
         browser = create_browser(p, False)
         context = browser.new_context()
@@ -104,20 +114,20 @@ def login():
         stealth_sync(page=page)
         page.goto("https://login.live.com/login.srf")
         input()
-        print("Saving browser cookies...")
+        logging.info("Saving browser cookies...")
         try:
             context.storage_state(path="cookies.json")
         except Error as e:
-            print(f"Error occurred while saving cookies: {e}")
+            logging.info(f"Error occurred while saving cookies: {e}")
             exit(1)
-        print("Cookies saved to cookies.json")
-        print("===========================================")
-        print("DO NOT SHARE THE COOKIES FILE WITH ANYONE!")
-        print(
+        logging.info("Cookies saved to cookies.json")
+        logging.info("===========================================")
+        logging.info("DO NOT SHARE THE COOKIES FILE WITH ANYONE!")
+        logging.info(
             "IT CONTAINS YOUR LOGIN INFORMATION, AND CAN BE USED TO ACCESS YOUR ACCOUNT!"  # noqa: E501
         )
-        print("===========================================")
-        print("Closing browser...")
+        logging.info("===========================================")
+        logging.info("Closing browser...")
         browser.close()
 
 
@@ -126,47 +136,45 @@ def get_score(page: Page, mobile: bool = False) -> int:
     Get the current score.
     """
     if mobile:
-        dbg_print("Mobile mode, opening drawer...")
+        logging.info("Mobile mode, opening drawer...")
         page.locator("#mHamburger").click()
         wait(0.5, 1.5)
         for _ in range(3):
-            dbg_print("Getting score...")
+            logging.info("Getting score...")
             try:
                 return int(page.locator("#fly_id_rc").inner_text(timeout=1000))
             except ValueError:
-                dbg_print("Error occurred while parsing score, waiting...")
+                logging.info("Error occurred while parsing score, waiting...")
                 wait(0.5, 1.5)
             except TimeoutError:
-                dbg_print("Timeout occurred while parsing score, opening drawer...")
+                logging.info("Timeout occurred while parsing score, opening drawer...")
                 page.locator("#mHamburger").click()
                 wait(0.5, 1.5)
         return -1
     else:
-        dbg_print("PC mode.")
+        logging.info("PC mode.")
         for _ in range(3):
-            dbg_print("Getting score...")
+            logging.info("Getting score...")
             try:
                 return int(page.locator("#id_rc").inner_text(timeout=1000))
             except ValueError:
-                dbg_print("Error occurred while parsing score, waiting...")
+                logging.info("Error occurred while parsing score, waiting...")
                 wait(0.5, 1.5)
         return -1
 
 
 def search(page: Page, mobile: bool = False):
-    print("Initializing word list...")
-    print("Starting search...")
     prev_score = -1
-    m_same_score = 0
+    same_score_count = 0
     for i in range(50):
-        print(f"Search attempt {i + 1}/50")
+        logging.info(f"Search attempt {i + 1}/50")
         word_len = randint(2, 10)
-        print("Word length:", word_len)
         words = []
         for _ in range(word_len):
             words.append(choice(list(WORD_LIST)))
-        print("Word:", words)
+        logging.info(f"Words: {words} ({word_len})")
         if mobile and i == 0:
+            logging.info("Simulating typing on first mobile search...")
             form_q = page.locator("#sb_form_c")
             form_q.click()
             wait(2, 3)
@@ -175,91 +183,95 @@ def search(page: Page, mobile: bool = False):
             page.keyboard.press("Enter")
         else:
             generated_query = "+".join(words)
-            print("Generated query:", generated_query)
             page.goto(f"https://www.bing.com/search?q={generated_query}&form=QBLH")
         wait(3, 4)
         curr_score = get_score(page, mobile)
-        print("Current score:", curr_score)
-        print("Previous score:", prev_score)
+        logging.info(f"Score (current / previous): {curr_score} / {prev_score}")
         if curr_score == -1:
-            print("Error occurred while parsing score, skipping...")
+            logging.info("Error occurred while parsing score, skipping...")
             continue
         if curr_score == prev_score:
-            if not mobile:
-                print("Score did not change, probably we searched enough.")
+            logging.info(f"Same score count: {same_score_count}")
+            if same_score_count == 3:
+                logging.info(
+                    "Score did not change 3 times, probably we searched enough."
+                )
+                logging.info(
+                    "If the score isn't full, please report this issue on GitHub."
+                )
                 break
-            else:
-                dbg_print("Mobile same score:", m_same_score)
-                if m_same_score == 3:
-                    print("Score did not change 3 times, probably we searched enough.")
-                    break
-                m_same_score += 1
+            same_score_count += 1
         prev_score = curr_score
-        dbg_pause()
-    print("Search complete.")
+        Debug.pause()
+    logging.info("Search complete.")
 
 
 def launch_pc(p: Playwright, silent: bool = False, force_chromium: bool = False):
-    print("Launching browser (PC version)...")
+    logging.info("Launching browser (PC version)...")
     if silent or force_chromium:
         browser = p.chromium.launch(headless=silent)
     else:
         browser = create_browser(p, silent)
-    print("Loading config & cookies...")
+    logging.info("Loading config & cookies...")
     edge = p.devices["Desktop Edge"]
     context = browser.new_context(**edge, storage_state="cookies.json")
     page = context.new_page()
     stealth_sync(page=page)
-    print("Visiting Bing...")
+    logging.info("Visiting Bing...")
     page.goto("https://www.bing.com/")
-    dbg_screenshot(page, "bing-chromium-1")
-    dbg_pause()
+    Debug.screenshot(page, "bing-chromium-1")
+    Debug.pause()
     wait(3, 5)
-    print("Clicking the 'Login' button...")
-    dbg_screenshot(page, "bing-chromium-2")
+    logging.info("Clicking the 'Login' button...")
+    Debug.screenshot(page, "bing-chromium-2")
     page.locator("#id_l").click()
     wait(2, 3)
-    print("Executing search function...")
+    logging.info("Executing search function...")
     search(page)
-    dbg_pause()
-    print("Closing browser...")
+    Debug.pause()
+    logging.info("Closing browser...")
     browser.close()
 
 
-def launch_mobile(p: Playwright, silent: bool = False, no_webkit: bool = False, force_chromium: bool = False):
-    print("Launching browser (Mobile version)...")
+def launch_mobile(
+    p: Playwright,
+    silent: bool = False,
+    no_webkit: bool = False,
+    force_chromium: bool = False,
+):
+    logging.info("Launching browser (Mobile version)...")
     if no_webkit:
         browser = p.chromium
     else:
         browser = p.webkit
-    # print(p.devices)
+    # logging.info(p.devices)
     iphone = p.devices["iPhone 13 Pro Max"]
-    print("Monkey-patching WebKit user agent...")
+    logging.info("Monkey-patching WebKit user agent...")
     iphone["user_agent"] = EDGE_IOS_UA
     if no_webkit and not force_chromium:
         browser = create_browser(p, headless=silent, browser_type=browser)
     else:
         browser = browser.launch(headless=silent)
-    print("Loading config & cookies...")
+    logging.info("Loading config & cookies...")
     context = browser.new_context(**iphone, storage_state="cookies.json")
     page = context.new_page()
     stealth_sync(page=page)
-    print("Visiting Bing...")
+    logging.info("Visiting Bing...")
     page.goto("https://www.bing.com/")
-    dbg_screenshot(page, "bing-webkit-1")
-    dbg_pause()
+    Debug.screenshot(page, "bing-webkit-1")
+    Debug.pause()
     wait(1, 2)
-    print("Opening the drawer...")
-    dbg_screenshot(page, "bing-webkit-2")
+    logging.info("Opening the drawer...")
+    Debug.screenshot(page, "bing-webkit-2")
     page.locator("#mHamburger").click()
     wait(1, 2)
-    print("Clicking the 'Login' button...")
+    logging.info("Clicking the 'Login' button...")
     page.locator("#hb_s").click()
     wait(1, 2)
-    print("Executing search function...")
+    logging.info("Executing search function...")
     search(page, mobile=True)
-    dbg_pause()
-    print("Closing browser...")
+    Debug.pause()
+    logging.info("Closing browser...")
     browser.close()
 
 
@@ -267,10 +279,10 @@ def install_deps():
     """
     Install required dependencies.
     """
-    print("Installing Playwright...")
+    logging.info("Installing Playwright dependencies...")
     sys.argv = ["playwright", "install", "chromium", "webkit"]
     run_module("playwright", run_name="__main__")
-    print("Dependencies installed.")
+    logging.info("Dependencies installed.")
 
 
 def main():
@@ -314,15 +326,21 @@ def main():
         action="store_true",
         help="Force use of Chromium even if Edge is available.",
     )
+    parser.add_argument(
+        "--no-stealth",
+        action="store_true",
+        help="Do not use playwright_stealth.",
+    )
     args = parser.parse_args()
     DEBUG = args.debug
-    print(f"Bingto {__version__} - https://github.com/teppyboy/bingto")
-    print("")
+    logging.info(f"Bingto {__version__} - https://github.com/teppyboy/bingto")
     if args.install:
         install_deps()
         exit()
+    if args.no_stealth:
+        init_fake_playwright_stealth()
     if not Path("cookies.json").exists():
-        print("Cookies file not found.")
+        logging.info("Cookies file not found.")
         login()
     with sync_playwright() as p:
         # PC
